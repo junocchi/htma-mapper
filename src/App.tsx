afa23eb6de99e714b2c02565dee256773363c18a
Juliana Nocchi <juliana.nocchi@spendesk.com>
2024-09-15 16:03:48 +0100

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { Footer } from "./components/Footer/Footer";
 import MineralsForm from "./components/MineralsForm";
 
 export default function App() {
   return (
     <>
       <MineralsForm />
+      <Footer />
     </>
   );
 }
